3f24248248d7d6503282ce4cf3961c9b9049172b
Marcy4000 <marcellomaggio07@gmail.com>
2025-01-05 17:53:36 +0100

    aggiunta risposta multipla

--- Assets/Scripts/Gameplay/MissionData.cs
+++ Assets/Scripts/Gameplay/MissionData.cs
@@ -12,6 +12,9 @@ public class MissionData : ScriptableObject
     public AnswerType expectedAnswerType; // Type of answer (e.g., "number", "text")
     public string expectedAnswer; // Expected answer to the question
 
+    public string[] risposteMultiple; // Multiple choice answers
+    public int rispostaCorretta; // Index of the correct answer
+
     public string link; // Link to additional information
 
     public Reward[] rewards; // Rewards for completing the mission
@@ -49,6 +52,8 @@ public class MissionData : ScriptableObject
                 return float.TryParse(answer, out float floatResult) && floatResult.ToString() == expectedAnswer;
             case AnswerType.Text:
                 return string.Equals(answer, expectedAnswer, StringComparison.OrdinalIgnoreCase);
+            case AnswerType.MultipleChoice:
+                return int.TryParse(answer, out int intResult2) && intResult2 == rispostaCorretta;
             default:
                 return false;
         }
@@ -79,5 +84,6 @@ public enum AnswerType
 {
     Integer,
     Float,
-    Text
+    Text,
+    MultipleChoice
 }
--- Assets/Scripts/Gameplay/MissionFormController.cs
+++ Assets/Scripts/Gameplay/MissionFormController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,6 +9,7 @@ public class MissionFormController : MonoBehaviour
     public TMP_Text missionTitleText; // Displays the mission title
     public TMP_Text missionQuestionText; // Displays the mission question
     public TMP_InputField answerInputField; // Field for the player's answer
+    public TMP_Dropdown answerDropdown; // Dropdown for multiple choice answers
     public Button submitButton;
     public ClickableLink link;
 
@@ -42,13 +44,26 @@ public class MissionFormController : MonoBehaviour
         switch (mission.expectedAnswerType)
         {
             case AnswerType.Integer:
+                answerInputField.gameObject.SetActive(true);
+                answerDropdown.gameObject.SetActive(false);
                 answerInputField.contentType = TMP_InputField.ContentType.IntegerNumber;
                 break;
             case AnswerType.Float:
+                answerInputField.gameObject.SetActive(true);
+                answerDropdown.gameObject.SetActive(false);
                 answerInputField.contentType = TMP_InputField.ContentType.DecimalNumber;
                 break;
             case AnswerType.Text:
+                answerInputField.gameObject.SetActive(true);
+                answerDropdown.gameObject.SetActive(false);
                 answerInputField.contentType = TMP_InputField.ContentType.Standard;
+                break;
+            case AnswerType.MultipleChoice:
+                answerInputField.gameObject.SetActive(false);
+                answerDropdown.gameObject.SetActive(true);
+                answerDropdown.ClearOptions();
+                answerDropdown.AddOptions(mission.risposteMultiple.ToList());
+                answerDropdown.value = 0;
                 break;
             default:
                 break;
@@ -62,7 +77,8 @@ public class MissionFormController : MonoBehaviour
 
     private void OnSubmit()
     {
-        string answer = answerInputField.text;
+        string answer = currentMission.expectedAnswerType == AnswerType.MultipleChoice ?
+            answerDropdown.value.ToString() : answerInputField.text;
         bool vaild = missionManager.CompleteMission(currentMission, answer);
         if (vaild)
         {
